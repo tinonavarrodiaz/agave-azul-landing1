dd517a237bc2f1b0e956af61c2e4a6a2ec075d55
Florentino Navarro <tino.navarro@hotmail.com>
2023-09-30 13:36:24 -0600

    error fixed

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/pages/Home";
-import PublicTemplate from "../components/templates/public/publicTemplate";
-// import PublicTemplate from "../components/templates/public/PublicTemplate";
+import PublicTemplate from "../components/templates/public/PublicTemplate";
 export const Router = createBrowserRouter([
   {
     path: '/',
